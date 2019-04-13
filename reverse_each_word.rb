c508145c9337fc500663d67a9c65e008d2d2db14
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-04-13 20:12:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(string)
   split_string = string.split("")
   reversed = []
   string.size.times { reversed << split_string.pop }
+  reversed.join
 end
 
 puts reverse_each_word("Hello there, and how are you?")
